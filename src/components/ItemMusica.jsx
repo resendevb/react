07f23330cbf3026c6bfcd14ecb293f60efdbb0c6
Beatriz <jaciana.resende@sptech.school>
2023-05-05 14:54:19 -0300

    projeto music box

---
+++ src/components/ItemMusica.jsx
@@ -0,0 +1,108 @@
+import api from "../api";
+import React from "react";
+import capaPadrao from "../html-css-template/imagens/capa.png";
+import editIcon from "../html-css-template/imagens/edit-icon.png";
+import deleteIcon from "../html-css-template/imagens/delete-icon.png";
+import { useState } from "react";
+
+function ItemMusica(props) {
+  const estiloCard = {
+    backgroundImage: `url(${props.capa ? props.capa : capaPadrao})`
+  };
+
+  const [nome, setNome] = useState(props.nome);
+  const [artista, setArtista] = useState(props.artista);
+  const [genero, setGenero] = useState(props.genero);
+  const [ano, setAno] = useState(props.ano);
+
+  const [editing, setEditing] = useState(false);
+
+  function atualizarMusica() {
+    const musicaAtualizada = {
+      nome,
+      artista,
+      genero,
+      ano
+    };
+
+    api
+      .put(`/${props.id}`, musicaAtualizada)
+      .then((res) => {
+        alert("MUSICA ATUALIZADA");
+      })
+      .catch((error) => {
+        alert("DEU ERRO, CHAMA O AMIGO");
+      });
+  }
+
+  return (
+    <>
+      <div className="card-music" style={estiloCard}>
+        <div className="icons">
+          <img src={editIcon}
+            alt="Icone de um lapis"
+            onClick={() => setEditing(true)}
+          />
+          <img src={deleteIcon} alt="" onClick={() => props.funcaoDeletar(props.id)}/>
+        </div>
+        <div className="info-music">
+          <p>
+            <strong className="card-title">música: </strong>
+            <input
+              type="text" defaultValue={nome}
+              disabled={!editing}
+
+              className={editing ? "input-music-enable"
+                : "input-music-disabled"}
+              onChange={(e) => setNome(e.target.value)}
+            />
+          </p>
+          <p>
+            <strong className="card-title">artista: </strong>
+            <input
+
+              type="text"
+              defaultValue={artista}
+              disabled={!editing}
+
+              className={editing ? "input-music-enable"
+                : "input-music-disabled"}
+              onChange={(e) => setArtista(e.target.value)}
+            />
+          </p>
+          <p>
+            <strong className="card-title">categoria: </strong>
+            <input
+
+              type="text"
+              defaultValue={genero}
+              disabled={!editing}
+
+              className={editing ? "input-music-enable"
+                : "input-music-disabled"}
+              onChange={(e) => setGenero(e.target.value)}
+            />
+          </p>
+          <p>
+            <strong className="card-title">ano: </strong>
+            <input
+
+              type="text"
+              defaultValue={ano}
+              disabled={!editing}
+
+              className={editing ? "input-music-enable"
+                : "input-music-disabled"}
+              onChange={(e) => setAno(e.target.value)}
+            />
+          </p>
+          <button onClick={atualizarMusica} className="btn-salvar-enable">
+            Salvar
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default ItemMusica;
